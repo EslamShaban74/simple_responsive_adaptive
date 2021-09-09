e2bd5027e7fa42c69c7dd53030f42d2630e4f032
Eslam <eslamshaban12328@gmail.com>
2021-09-09 18:57:22 +0200

    responsive and adaptive

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
   runApp(MyApp());
 }
 
+// hello test ..
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,7 +16,23 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MobileScreen(),
+      home: Builder(
+        builder: (BuildContext context)
+        {
+          if(MediaQuery.of(context).size.width.toInt() <= 560)
+            return MediaQuery(
+                data: MediaQuery.of(context).copyWith(
+                  textScaleFactor: 0.7,
+                ),
+                child: MobileScreen());
+
+          return MediaQuery(
+              data: MediaQuery.of(context).copyWith(
+                textScaleFactor: 1.25,
+              ),
+              child: DesktopScreen());
+        },
+      ),
     );
   }
 }
